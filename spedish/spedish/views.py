1dde2ff82b694f2e3a8ac7e7766b0374cd4117a8
James Xu <james.xu@spedish.com>
2015-09-03 00:28:38 -0700

    Correctly removing the NullSerializer

--- spedish/spedish/views.py
+++ spedish/spedish/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from spedish.serializers import NullSerializer, UserSerializer
+from spedish.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
