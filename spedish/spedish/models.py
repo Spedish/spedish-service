33e9c9068302d107e3214d5152257e976e3fcc17
Huaxi Wang <huaxi.wang@gmail.com>
2015-09-05 02:32:19 -0700

    Added address model and migration script, added address to user creation api.

--- spedish/spedish/models.py
+++ spedish/spedish/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from localflavor.us.models import USStateField, USZipCodeField
 
-class UserProfile(models.Model): 
+
+class Address(models.Model):
+    """
+    This model holds the user address
+    """
+    line_one = models.CharField(max_length= 100)
+    line_two = models.CharField(max_length = 100, blank=True)  # line_two is optional
+    city = models.CharField(max_length = 50)
+    state = USStateField()
+    zip_code = USZipCodeField()
+
+
+class UserProfile(models.Model):
     """
     This model holds the user profile related to a particular user
     """
     # Each profile corresponds to exactly 1 users
     user = models.OneToOneField(User)
-    
     isSeller = models.BooleanField()
+    address = models.OneToOneField(Address, default = '')
+
+
+
