0a4efb7344aadc6575de1e32cbbbb0ce139b62f5
James Xu <james.xu@spedish.com>
2015-09-10 20:37:31 -0700

    Added PUT (update) operation for UserProfile:  - Read/Write/Update serializers  - New PUT operation  - Update existing records, create new address if added additional

--- spedish/spedish/serializers.py
+++ spedish/spedish/serializers.py
@@ -7,33 +7,53 @@ from spedish.models import UserProfile, Address
 from django.db import transaction
 
 
-class UserAddressSerializer(serializers.ModelSerializer):
+class UserAddressReadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = ('id', 'line_one', 'line_two', 'city', 'state', 'zip_code')
+
+
+class UserAddressWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ('line_one', 'line_two', 'city', 'state', 'zip_code')
+
+
+class UserAddressUpdateSerializer(serializers.ModelSerializer):
+    entry_id = serializers.IntegerField()
+
+    class Meta:
+        model = Address
+        fields = ('entry_id', 'line_one', 'line_two', 'city', 'state', 'zip_code')
 
 
 class UserAuthSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password')
-        
-        
+
+
 class UserReadSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
-        
-        
+
+
 class UserWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'first_name', 'last_name', 'email')
-        
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
 
 class UserProfileReadSerializer(serializers.ModelSerializer):
     user = UserReadSerializer(many = False, read_only = True)
-    address = UserAddressSerializer(many = True, read_only = True)
+    address = UserAddressReadSerializer(many = True, read_only = True)
 
     class Meta:
         model = UserProfile
@@ -42,12 +62,12 @@ class UserProfileReadSerializer(serializers.ModelSerializer):
 
 class UserProfileWriteSerializer(serializers.ModelSerializer):
     user = UserWriteSerializer(many = False, read_only = False)
-    address = UserAddressSerializer(many = True, read_only = False)
+    address = UserAddressWriteSerializer(many = True, read_only = False)
 
     class Meta:
         model = UserProfile
         fields = ('user', 'isSeller', 'address')
-        
+
     @transaction.atomic
     def create(self, validated_data):
         data = copy.deepcopy(validated_data)
@@ -62,10 +82,55 @@ class UserProfileWriteSerializer(serializers.ModelSerializer):
         userProfileRec.save()
 
         for item in address:
+            # id field in data is used for looking up address only
+            if 'id' in item:
+                del item['id']
             userAddress = Address(user = userProfileRec, **item)
             userAddress.save()
-        
+
         return userProfileRec
 
+
+class UserProfileUpdateSerializer(serializers.ModelSerializer):
+    user = UserUpdateSerializer(many = False, read_only = False)
+    address = UserAddressUpdateSerializer(many = True, read_only = False)
+
+    class Meta:
+        model = UserProfile
+        fields = ('user', 'address')
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        # UserProfile is made up of User, UserProfile and Address, we unpack
+        # the data for each first
+        data = copy.deepcopy(validated_data)
+        userData = data.pop('user')
+        addrList = data.pop('address')
+
+        # Update User
+        userRec = instance.user
+        self._setFields(userRec, userData, [])
+        userRec.save()
+
+        # Update the address list
+        for addr in addrList:
+            addrRec = instance.address.get(id=addr.get('entry_id'))
+            if not addrRec:
+                # insert a new address if the entry currently does not exist
+                del addr['entry_id']
+                addrRec = Address(user=instance, **addr)
+                addrRec.save()
+
+            self._setFields(addrRec, addr, [])
+            addrRec.save()
+
+        # Update UserProfile
+        # TODO: There are no other fields so nothing is written
+        return instance
+
+    def _setFields(self, instance, data, ignoreField):
+        for attr, val in data.items():
+            if attr not in ignoreField:
+                setattr(instance, attr, val)
 
 
